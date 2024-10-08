0d4ea6fc6076dd1b03f5c5ee9e33a3d64019d3a2
Trisa Abdul Hakim <hakimpbg@gmail.com>
2024-10-08 17:48:26 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', DashboardController::class);
+// Route::get('/', DashboardController::class);
